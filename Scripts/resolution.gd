06fa2813ac86b7f6eb8e8ded06792d70245c7d1d
StaringLongingly <dimbous@proton.me>
2025-10-08 21:50:44 +0300

    Init, Torso and Head done

---
+++ Scripts/resolution.gd
@@ -0,0 +1,28 @@
+extends Node
+
+@onready var window: Window = get_window() 
+@onready var base_size: Vector2i = window.content_scale_size
+
+@export var is_widescreen: bool = false; 
+var min_size = Vector2(640, 480)
+
+func _ready():
+	window.size_changed.connect(_on_window_size_changed) 
+	min_size.x = ProjectSettings.get_setting("display/window/size/viewport_width")
+	min_size.y = ProjectSettings.get_setting("display/window/size/viewport_height")
+	
+	window.min_size = Vector2i(min_size)
+	_on_window_size_changed()
+
+func _on_window_size_changed():
+	var clamped_size = window.size
+	var scale = clamped_size / base_size
+	var scale_size = window.size / (scale.y if scale.y <= scale.x else scale.x)
+	var scale_3d = min_size.y / clamped_size.y
+	print("Window Updated!:")
+	print("   New Window Size: " + str(clamped_size))
+	print("   Scale Size     : " + str(scale_size))
+	print("   3d Scale       : " + str(scale_3d))
+	$Fate/SubViewportContainer/SubViewport.scaling_3d_scale = scale_3d
+	# window.content_scale_size = scale_size;
+	if is_widescreen: window.content_scale_aspect = Window.CONTENT_SCALE_ASPECT_KEEP_WIDTH
